b5633acc6e4cd96877834633fe0b78c5167b4ea9
WIN_NT\x16j056 <x16j056@chiba-fjb.ac.jp>
2017-04-26 12:14:24 +0900

    カレンダー表示

--- Program.cs
+++ Program.cs
@@ -10,6 +10,48 @@ namespace Calendar
     {
         static void Main(string[] args)
         {
+            /*
+             * 現在の日時から、今月のカレンダーを画面に
+             * 表示するプログラム
+             */
+            DateTime dateNow = DateTime.Now;    //現在の日時を取得する
+
+            //現在の年、月を画面に表示する
+            Console.Write("{0}年 {1}月\n", dateNow.Year, dateNow.Month);
+
+            //現在の年月の1日を取得する
+            DateTime dateFirst = DateTime.Parse(string.Format("{0}/{1}/1", dateNow.Year, dateNow.Month));
+            DisplayCalendar(dateFirst);
+            Console.ReadLine();
+        }
+
+        //1ヶ月分のカレンダーを画面に表示する
+        //パラメータ
+        //  dateFirst:表示したい年月の1日を表すDateTime
+        static void DisplayCalendar(DateTime dateFirst)
+        {
+            int week;
+            int month = dateFirst.Month;
+
+            //曜日タイトルの表示
+            Console.Write("--------------------\n");
+            Console.Write("日 月 火 水 木 金 土\n");
+
+            //1日の開始位置まで空白を表示する
+            for (week = 0; week < (int)dateFirst.DayOfWeek; week++)
+                Console.Write("   ");
+
+            //月の終わりも日まで1週間ごとに日にちを表示する
+            while (month == dateFirst.Month)
+            {
+                for (; week < 7 && month == dateFirst.Month; week++)
+                {
+                    Console.Write("{0, 2} ", dateFirst.Day);
+                    dateFirst = dateFirst.AddDays(1);
+                }
+                Console.Write("\n");
+                week = 0;
+            }
         }
     }
 }
